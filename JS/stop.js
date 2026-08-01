00b879150d571eb8c21dd3a5d6153edcc2d533c9
GigaPixel Entertainment <entertainmentgigapixel@gmail.com>
2026-07-31 23:23:42 -1000

    refactor

--- JS/stop.js
+++
@@ -1,8 +0,0 @@
-console.error(`
- _____ _____ ___________ 
-/  ___|_   _|  _  | ___ \\
-\\ \`--.  | | | | | | |_/ /
- \`--. \\ | | | | | |  __/ 
-/\\__/ / | | \\ \\_/ / |    
-\\____/  \\_/  \\___/\\_|    `)
-console.error("DO NOT PASTE ANYTHING INSIDE OF DEVTOOLS. It may cause you to permanently lose your account!")
